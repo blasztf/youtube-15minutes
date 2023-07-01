3ee086a1aade19988150be6527d0b9860ba3bb70
Blaszt Fire <ywhy1103@gmail.com>
2023-07-01 08:31:36 +0700

    Update video_prepare.py

--- video_prepare.py
+++ video_prepare.py
@@ -2,12 +2,14 @@
 import os
 import subprocess
 import json
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 
 from yt15m.model.video import *
 from yt15m.repository.video import VideoRepository
 
 from yt15m.util.helper import use_hook
+
+import traceback as backtrack
 
 PROGRESS_DONE = "Done"
 
@@ -39,15 +41,16 @@ def execute_cmd(vm, verbose=False, cookie_login=None, chromedriver=None, show_wb
         "--video-vid", str(vm.vid),
         "--video-progress", str(vm.progress),
         "--uploader", 'web',
+        "--show-web-browser" if show_wb else "",
         "--cookie-login-path", cookie_login,
-        "--chromedriver-path", chromedriver,
-        ("--show-web-browser" if show_wb else "")
+        "--chromedriver-path", chromedriver
     ]
 
     result = True
     try:
         execute(cmd)
     except:
+        backtrack.print_exc()
         result = False
     
     return result
@@ -67,7 +70,15 @@ def check_list_datastore(set_list_datastore):
         f.close()
     if not os.path.exists(os.path.join(path_template, "series.txt")):
         f = open(os.path.join(path_template, "series.txt"), mode='w')
-        f.write("your series title.")
+        f.writelines([
+            "TITLE", "=", '"Insert series title."', "\n",
+            "DESCRIPTION", "=", '"Insert series description."', "\n",
+            "CATEGORY", "=", '"Insert series category. (ex. 22 for game)"', "\n",
+            "KEYWORDS", "=", '"Insert series keywords. (ex. keyword 1,keyword 2,...keyword n)"', "\n",
+            "PLAYLIST", "=", '"Insert series youtube playlist id."', "\n",
+            "PRIVACY", "=", '"public"', "\n",
+            "FORKIDS", "=", '0'
+        ])
         f.close()
     sources = os.path.join(path_template, 'sources')
     os.makedirs(sources, exist_ok=True)
@@ -98,7 +109,10 @@ def check_list_datastore(set_list_datastore):
     if not ok:
         return False
 
-    list_datastore.append(repo_single)
+    vm = repo_single.all()[0]
+    if vm.title is not None:
+        list_datastore.append(repo_single)
+
     # For series, fetch all repo except template
     for series in os.listdir(path_series):
         if series == template_series:
@@ -110,15 +124,20 @@ def check_list_datastore(set_list_datastore):
             counter_f = open(os.path.join(series, 'counter.txt'), mode='r')
             counter_m = int(counter_f.readline())
             counter_f.close()
-            series_f = open(os.path.join(series, 'series.txt'), mode='r')
-            series_m = series_f.readline()
-            series_f.close()
+            series_m = dotenv_values(os.path.join(series, 'series.txt'))
             builder = None
             
             for model in repo.all():
                 builder = VideoModelBuilder(model)
                 if not model.title:
-                    builder.title(f"{series_m} {counter_m}").file(os.path.join(series, 'sources', f"video{counter_m}.mp4"))
+                    builder.title(f"{series_m['TITLE']} {counter_m}")
+                    builder.file(os.path.join(series, 'sources', f"video{counter_m}.mp4"))
+                    builder.description(series_m['DESCRIPTION'])
+                    builder.category(series_m['CATEGORY'])
+                    builder.keywords(series_m['KEYWORDS'])
+                    builder.playlist(series_m['PLAYLIST'])
+                    builder.privacy_status(series_m['PRIVACY'])
+                    builder.is_for_kids(series_m['FORKIDS'])
                     counter_m += 1
                     repo.update(builder.build())
             
@@ -145,15 +164,18 @@ def main():
         builder = None
         for repo in list_datastore[0]:
             list_vm = repo.all()
-            for vm in list_vm:
-                if vm.progress != PROGRESS_DONE:
-                    print(f"Processing '{vm.title}'...")
-                    if execute_cmd(vm, verbose=verbose, cookie_login=cookie_login, chromedriver=chromedriver, show_wb=show_wb):
-                        builder = VideoModelBuilder(vm)
-                        builder.progress(PROGRESS_DONE)
-                        repo.update(builder.build())
-                    else:
-                        break
+            try:
+                for vm in list_vm:
+                    if vm.progress != PROGRESS_DONE:
+                        print(f"Processing '{vm.title}'...")
+                        if execute_cmd(vm, verbose=verbose, cookie_login=cookie_login, chromedriver=chromedriver, show_wb=show_wb):
+                            builder = VideoModelBuilder(vm)
+                            builder.progress(PROGRESS_DONE)
+                            repo.update(builder.build())
+                        else:
+                            raise Exception()
+            except:
+                break
     pass
 
 if __name__ == "__main__":
